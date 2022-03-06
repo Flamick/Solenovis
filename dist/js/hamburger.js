fb02f0352e6e4b099c38984a1778db5f100c9f64
Flamick <62188033+Flamick@users.noreply.github.com>
2022-03-06 13:17:43 -0500

    update

--- dist/js/hamburger.js
+++ dist/js/hamburger.js
@@ -2,6 +2,7 @@
 const menuBtn = document.querySelector('.menu-btn');
 const mobileMenu = document.querySelector('#mobile-menu');
 const menuLabel = document.querySelector('#menu-label');
+const facebookIcon = document.querySelector('.fa-facebook');
 let toggle = false;
 menuBtn.addEventListener('click', () => {
     menuToggle();
@@ -16,11 +17,13 @@ function menuToggle () {
         menuBtn.classList.add('open');
         mobileMenu.classList.add('open');
         menuLabel.classList.add('hidden');
+        facebookIcon.classList.add('hidden');
         toggle = true;
     } else {
         menuBtn.classList.remove('open');
         mobileMenu.classList.remove('open');
         menuLabel.classList.remove('hidden');
+        facebookIcon.classList.remove('hidden');
         toggle = false;
     }
 };
